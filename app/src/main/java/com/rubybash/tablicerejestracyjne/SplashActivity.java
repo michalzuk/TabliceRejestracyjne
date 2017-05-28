82eb262fadc992a09c78e1d586a186822e407579
michalzuk <michalzuk@hotmail.com>
2017-05-28 21:58:21 +0200

    SplashActivity rework

--- app/src/main/java/com/rubybash/tablicerejestracyjne/SplashActivity.java
+++ app/src/main/java/com/rubybash/tablicerejestracyjne/SplashActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.rubybash.tablicerejestracyjne.MainActivity;
-
 public class SplashActivity extends AppCompatActivity {
 
     @Override
